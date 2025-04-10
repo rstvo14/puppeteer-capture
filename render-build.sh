e60d667d6343d58cd4e20bf448e153162326c323
Gustavo Rodiguez Salas <rstvo14@gmail.com>
2025-04-09 18:02:36 -0600

    GS: clean

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-# Clean install - force remove puppeteer-core in case it's cached
 npm uninstall puppeteer-core || true
-
-# Clear any package-lock file that may be locking wrong versions
-rm -f package-lock.json
-
-# Reinstall puppeteer from scratch
 npm install puppeteer@24.6.1 --force
-
-# Set up Chromium in a known cache dir
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 npx puppeteer browsers install chrome
